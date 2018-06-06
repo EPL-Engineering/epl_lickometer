87822c9b977352a71a8f56cd6a973840b2c7a917
Evan Foss <evanfoss@gmail.com>
2018-06-06 14:27:42 -0400

    initial commit

---
+++ lick.cmd
@@ -0,0 +1,42 @@
+# Pin name action command file
+
+# Start of element CONN2
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 3, 3)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element V1
+ChangePinName(V1, 2, -)
+ChangePinName(V1, 1, +)
+
+# Start of element CONN1
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element Z1
+ChangePinName(Z1, 1, 1)
+ChangePinName(Z1, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, S, S)
+ChangePinName(Q1, D, D)
+ChangePinName(Q1, G, G)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
